b1ecd8e3f3d9366b29fd2c6e3f5e6a30c6dce37c
noahwwwwww232wed <ng.www10@gmail.com>
2025-05-14 10:56:47 +0200

    Add bcrypt, express, and mysql dependencies to package.json

--- node_backend/routes/adminRoutes.js
+++ node_backend/routes/adminRoutes.js
@@ -1,28 +1,27 @@
+const mysql = require('mysql2/promise');
 
 async function register(req, res) {
-  // req is voor de data vanuit de frontend (bijvoorbeeld uit een formulier)
-  // res is voor de data die je terug stuurt naar de frontend
-  // res.send("welkom bij de admin pagina");
- // res.sendFile(path.join(__dirname, '../views/admin.html'));
- console.log('Probeer verbinding te maken met de database...');
-   try {
-     const connection = await mysql.createConnection({
-       host: 'localhost',
-       user: 'root',
-       password: '',
-       database: 'deveenhoop'
-     });
- 
-     // Execute a simple query
-     //insert query
-     const [rows, fields] = await connection.execute('SELECT * FROM users');
-     console.log('Query results:', rows);
- 
-     await connection.end();
-   } catch (error) {
-     console.error('Database connectie is mislukt:', error);
-   }
+  console.log('Probeer verbinding te maken met de database...');
+
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'deveenhoop'
+    });
+
+    const [rows, fields] = await connection.execute('SELECT * FROM users');
+    console.log('Query results:', rows);
+
+    await connection.end();
+
+   
+    return res.send("Welkom bij de admin pagina. Data opgehaald, zie console.");
+  } catch (error) {
+    console.error('Database connectie is mislukt:', error);
+    return res.status(500).send("Database connectie is mislukt.");
+  }
 }
 
-// je moet hem exporteren omdat je hem ander niet kan importeren bij anderre bestanden
-module.exports = {getAdmins};
+module.exports = { register };
