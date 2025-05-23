b0fedf231da6c579481024406c45bf3cb213d116
noahwwwwww232wed <ng.www10@gmail.com>
2025-05-23 11:40:58 +0200

    Add node-gyp-build module with necessary files and configurations
    - Introduced the node-gyp-build module to facilitate building and loading native bindings with support for prebuilds.
    - Added essential files including LICENSE, README.md, SECURITY.md, and various JavaScript files for functionality.
    - Implemented scripts for installation and testing, ensuring compatibility with both Node.js and Electron environments.
    - Configured package.json with dependencies, scripts, and metadata for proper module management.

--- node_backend/routes/loginRoutes.js
+++ node_backend/routes/loginRoutes.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2/promise');
+const bcrypt = require('bcrypt');
 
 async function login(req, res) {
 
@@ -6,25 +7,30 @@ async function login(req, res) {
 
   try {
     const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'deveenhoop'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     });
 
     console.log('Verbonden met de database!');
 
     //check met select of req.body.email en password in de database zitten
-    const [rows, fields] = await connection.execute('SELECT * FROM users WHERE email = ? AND password = ?', [req.body.email, req.body.password]);
-    console.log('rows: ', rows);
-    await connection.end();
-
-    if (rows.length == 0) {
+    const [rows, fields] = await connection.execute('SELECT * FROM users WHERE email = ?', [req.body.email]);
+    if (rows.length === 0) {
       return res.status(401).send('Gebruikersnaam of wachtwoord is onjuist');
-    } else {
-      return res.status(200).send(rows);
     }
 
+    const user = rows[0];
+    await connection.end();
+
+    bcrypt.compare(req.body.password, user.password, function (err, result) {
+      console.log(result);
+      if (err) { throw (err); }
+      else {
+        return res.status(200).send(JSON.stringify('Inloggen is gelukt!'));
+      }
+    });
   } catch (error) {
     var errorMessage = JSON.stringify('Database connectie is mislukt:' + error);
     console.error(errorMessage)
